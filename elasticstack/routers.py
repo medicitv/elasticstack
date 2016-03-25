45cd728cb11fc35d72bd60210c86d476a56e36c3
Hugo Chargois <hchargois@medici.tv>
2016-03-25 17:02:03 +0100

    Allow subclassing MultilingualRouter to change prefix

--- elasticstack/routers.py
+++ elasticstack/routers.py
@@ -5,12 +5,14 @@ from haystack.constants import DEFAULT_ALIAS
 
 
 class MultilingualRouter(routers.BaseRouter):
+    prefix = DEFAULT_ALIAS
+
     def for_write(self, **hints):
-        return filter(lambda c: c.startswith(DEFAULT_ALIAS), settings.HAYSTACK_CONNECTIONS)
+        return filter(lambda c: c.startswith(prefix), settings.HAYSTACK_CONNECTIONS)
 
     def for_read(self, **hints):
         lang = translation.get_language()[:2]
-        localized_conn = "{}_{}".format(DEFAULT_ALIAS, lang)
+        localized_conn = "{}_{}".format(prefix, lang)
         if localized_conn in settings.HAYSTACK_CONNECTIONS:
             return localized_conn
-        return DEFAULT_ALIAS
+        return prefix
